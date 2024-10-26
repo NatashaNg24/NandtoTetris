4741e561994ebfed8582ce48ca44edee592ca1de
Natasha Ng <natasha.ng246@gmail.com>
2024-10-26 13:09:21 +0800

    Update Fill.asm

--- project 4: ASM/Fill.asm
+++ project 4: ASM/Fill.asm
@@ -1,5 +1,12 @@
-// This program fills the screen with black pixels if any key board key is pressed
-// When no key is pressed, the program whitens the screen
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/4/Fill.asm
+
+// Runs an infinite loop that listens to the keyboard input. 
+// When a key is pressed (any key), the program blackens the screen,
+// i.e. writes "black" in every pixel. When no key is pressed, 
+// the screen should be cleared.
 
 @place
 M=0     // set starting point
